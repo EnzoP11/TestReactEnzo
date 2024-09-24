738ae9bfb89a246e1742b9529057a922b7dd0035
EnzoP11 <enzoperriatpro@gmail.com>
2024-09-24 09:49:43 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,23 @@ import User from './User.jsx'
 
 function App() {
   
-
+    const feliksZemdegs = {
+      name: 'Feliks Zemdegs',
+      recordSingle : 4.16,
+      recordAverange : 5.53,
+      image :"https://images.thewest.com.au/publication/C-8421489/c2762e016da3b9684efc8bcd6030608b91a5a378.jpg"
+    }
+    const tymon = {
+      name: 'Tymon Kolasiński',
+      recordSingle : 3.66,
+      recordAverange : 4.74,
+      image :"https://i0.wp.com/efe.com/wp-content/uploads/2024/07/3CUBO-RUBIK.jpg?fit=1920%2C1280&ssl=1"
+    }
   return (
     <>
-      <User/>
+    <User {...feliksZemdegs}/>
+    <User {...tymon}/>
+      
     </>
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,17 +1,24 @@
 
 
-const User = () => {
+const User = (props) => {
 
-const myName = 'Enzo'
+
+/*const myName = 'Enzo'
 const myLastName = 'Perriat'
 const myAge = 22
-const isAuthenticated = true
+const isAuthenticated = true*/
 
     return (
-        <>
+        /*<>
         {isAuthenticated === true ? <h1>Vous êtes connecté</h1> : <h1>Connectez vous</h1>}
         <h1>Bonjour, mon prenom est {myName} et mon nom est {myLastName}</h1>
         <p>Je suis passionner de Rubik's Cube, d'échecs, et de jeux en tout genre. J'ai {myAge} ans.</p>
+        </>*/
+        <>
+        
+        <h1>Mon nom est {props.name}</h1>
+        <p>Mon meilleur temps au Rubiks Cube est : {props.recordSingle} et ma meilleur moyenne est : {props.recordAverange}</p>
+        <img src={props.image}/>
         
         </>
     )
